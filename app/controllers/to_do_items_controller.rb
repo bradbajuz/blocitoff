36f809e36869e2b64738f05d8906f1b8ece7c0b7
bradbajuz <brad@bradballard.com>
2014-04-06 23:03:39 -0400

    Update view

--- app/controllers/to_do_items_controller.rb
+++ app/controllers/to_do_items_controller.rb
@@ -1,5 +1,7 @@
 class ToDoItemsController < ApplicationController
   def index
+    @to_do_item = ToDoItem.new
+    @to_do_items = ToDoItem.all
     @to_do_items = current_user.to_do_items
   end
 end
--- app/models/to_do_item.rb
+++ app/models/to_do_item.rb
@@ -2,4 +2,9 @@ class ToDoItem < ActiveRecord::Base
 
   belongs_to  :user
 
+  def days_left
+    Time.now - created_at
+    # where("created_at <= ?", Time.now - 7.days).order("created_at asc")
+  end
+
 end
