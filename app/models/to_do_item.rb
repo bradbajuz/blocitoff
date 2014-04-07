3ef3e3c4ed2d8fb9f0e7eaaacef21cbcf60c2648
bradbajuz <brad@bradballard.com>
2014-04-06 23:13:17 -0400

    Update model

--- app/models/to_do_item.rb
+++ app/models/to_do_item.rb
@@ -2,9 +2,4 @@ class ToDoItem < ActiveRecord::Base
 
   belongs_to  :user
 
-  def days_left
-    Time.now - created_at
-    # where("created_at <= ?", Time.now - 7.days).order("created_at asc")
-  end
-
 end
