3810829e2c597946618acee7b9dbeeb021f4a8c0
bradbajuz <brad@bradballard.com>
2014-04-08 23:02:36 -0400

    Fix validation

--- app/controllers/to_do_items_controller.rb
+++ app/controllers/to_do_items_controller.rb
@@ -8,8 +8,9 @@ class ToDoItemsController < ApplicationController
   end
 
   def create
-    @to_do_item = ToDoItem.new
-    @to_do_item = current_user.to_do_items.build(to_do_item_params)
+    @to_do_item = ToDoItem.new(to_do_item_params)
+    # @to_do_item = current_user.to_do_items.build(to_do_item_params)
+    @to_do_item.user = current_user
 
     authorize @to_do_item
 
@@ -17,7 +18,9 @@ class ToDoItemsController < ApplicationController
       redirect_to to_do_items_path, notice: "Item was saved successfully."
     else
       flash[:error] = "Error creating todo. Please try again."
-      redirect_to @to_do_item
+      # puts "*** #{@to_do_item.errors.to_yaml}"
+      @to_do_items = current_user.to_do_items
+      render "to_do_items/index"
     end
   end
 
